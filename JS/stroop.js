1cf3d0b58a210f971e51e6cc047aba0ecacf4db4
Hanok <hanok_k92@yahoo.co.in>
2014-10-28 01:28:27 +0530

    Final changes to the illiterates

--- JS/stroop.js
+++ JS/stroop.js
@@ -165,9 +165,9 @@ function reload () {
   location.reload();
 }
 
-var Result=new Array([1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],
-                     [11,0],[12,0],[13,0],[14,0],[15,0],[16,0],[17,0],[18,0],[19,0],[20,0],
-                     [21,0],[22,0],[23,0],[24,0],[25,0],[26,0],[27,0],[28,0],[29,0],[30,0]);
+var Result=new Array([1,0,0],[2,0,0],[3,0,0],[4,0,0],[5,0,0],[6,0,0],[7,0,0],[8,0,0],[9,0,0],[10,0,0],
+                     [11,0,0],[12,0,0],[13,0,0],[14,0,0],[15,0,0],[16,0,0],[17,0,0],[18,0,0],[19,0,0],[20,0,0],
+                     [21,0,0],[22,0,0],[23,0,0],[24,0,0],[25,0,0],[26,0,0],[27,0,0],[28,0,0],[29,0,0],[30,0,0]);
 
 var lastColorIndex="red";
   var  lastTextIndex="RED";
@@ -221,7 +221,7 @@ function Questions (){
 
   lastColorIndex = colorIndex;
   lastTextIndex = textIndex;
-   a = performance.now();
+  a = performance.now();
 }
 
 function Record (Answer) {
@@ -230,8 +230,9 @@ function Record (Answer) {
   {
     Result[Count-5][1]=Math.round(((b-a)/1000)*100)/100;
     TotalTime=TotalTime+(b-a)/1000;
-    if (document.getElementById(Answer).innerHTML==TEXT[lastColorIndex]) {
+    if (document.getElementById(Answer).style.backgroundColor==COLORS[lastColorIndex]) {
         Score+=1;
+        Result[Count-5][2]=1;
     }
   }
   Count+=1;
